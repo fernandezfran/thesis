101461e1f3bbbe17049d96287e8ec61f1e7bb34e
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-04-04 16:09:23 -0300

    diagrama de flujo de dinámica molecular y grafico de potencial lj

---
+++ imagenes/metodos/lj.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0.8, 4, 500)
+
+xinv6 = 1 / x ** 6
+xinv12 = 1 / x ** 12
+lj = 4 * (xinv12 - xinv6)
+
+plt.xticks([0, 1, 2, 3, 4])
+plt.xlim((0, 4))
+plt.ylim((-1.5, 3.5))
+plt.xlabel(r"r/$\sigma$")
+plt.ylabel(r"V$_{LJ}$/$\varepsilon$")
+
+plt.axhline(y=0, color="k", linestyle='--', linewidth=1)
+plt.plot(x, lj, color="#5E81AC", linewidth=2)
+
+plt.savefig("lj.png", dpi=600)
