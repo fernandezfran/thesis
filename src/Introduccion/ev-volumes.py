a68e0899e291f04a4ee0e03fdf0d5ed4e121f167
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-10-31 17:09:32 -0300

    introduccion init

---
+++ src/Introduccion/ev-volumes.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+years = np.arange(2014, 2024)
+sales = 1_000 * np.array([320, 543, 791, 1262, 2082, 2276, 3245, 6768, 10532, 14000])
+
+fig, ax = plt.subplots()
+
+ax.bar(years, sales, width=0.6)
+
+ax.set_xticks(years)
+ax.set_xlabel("Año")
+ax.set_ylabel("Ventas anuales de vehículos eléctricos")
+
+fig.tight_layout()
+fig.savefig("ev-volumes.png", dpi=600)
+plt.show()
---
+++ src/Introduccion/iea-Li.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "year": [2022, 2025, 2030, 2035, 2040, 2045, 2050],
+        "ev": [69.8, 145.3, 349.5, 671.3, 907.1, 1005.7, 1039.5],
+        "es": [3.85, 7.68, 18.5, 30.47, 44.89, 47.78, 49.68],
+        "ed": [57.0, 74.0, 83.0, 92.0, 101.0, 111.0, 120.0],
+    }
+)
+
+
+categorias = [
+    "Vehículos eléctricos",
+    "Almacenamiento en baterías de red",
+    "Otras aplicaciones",
+]
+heights = [df["ev"], df["es"], df["ed"]]
+
+fig, ax = plt.subplots()
+
+ax.bar(df["year"], heights[0], label=categorias[0], width=1.5)
+for i in range(1, len(categorias)):
+    ax.bar(df["year"], heights[i], width=1.5, bottom=sum(heights[:i]), label=categorias[i])
+
+ax.set_xticks(df["year"])
+ax.set_xlabel("Año")
+ax.set_ylabel("Demanda total de litio (kt)")
+ax.legend()
+
+fig.tight_layout()
+fig.savefig("iea-Li.png", dpi=600)
+plt.show()
