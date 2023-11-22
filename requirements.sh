0cf602bf33308f6549cd4c0acf4b07c5e35d7015
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-11-22 14:23:16 -0300

    requirements and Makefile

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,13 @@
-apt update 
-apt upgrade
-apt-get install build-essential -y
-apt install texlive-latex-extra -y
-apt-get install latexmk -y
+#!/bin/bash
+# update and upgrade
+apt update && apt upgrade -y
+
+# install build-essential to use the make cmd
+apt install build-essential -y
+
+# minimal installation of LaTeX: base version and extra packages required
+apt install texlive-base -y
+apt install texlive-xetex
+apt install texlive-fonts-extra -y
+apt install texlive-lang-spanish -y
+apt install latexmk -y
