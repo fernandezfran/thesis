043213ac1c613bf49c2f7b2109ef3f184d75d3c2
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-11-24 16:45:58 -0300

    c-Si y a-Si en RDF del modelo DFTB

---
+++ src/Silicio/modelo/resultados/rdf/datos/config.tcl
@@ -0,0 +1,18 @@
+display projection Orthographic
+
+axes location off
+
+color Display Background white
+
+molinfo 0 set a 10.937456
+molinfo 0 set b 10.937456
+molinfo 0 set c 10.937456
+
+pbc wrap
+
+pbc box -color gray
+
+color change rgb blue 0.121569 0.466667 0.705882
+color Name S blue
+
+mol representation VDW
