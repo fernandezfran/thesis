b32a04915c8f7513ba596f19d5149db48697f657
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-11-30 20:13:11 -0300

    metodos py

--- src/Metodos/atomicos/lj.py
+++ src/Metodos/atomicos/lj.py
@@ -7,21 +7,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0.8, 4, 500)
-
-xinv6 = 1 / x ** 6
-xinv12 = 1 / x ** 12
-lj = 4 * (xinv12 - xinv6)
-
 plt.rcParams.update({"font.size": 12})
 
-plt.xticks([0, 1, 2, 3, 4])
+dist = np.linspace(0.8, 4, 500)
+epot = 4 * (1 / np.power(dist, 12) - 1 / np.power(dist, 6))
+
+plt.plot(dist, epot, linewidth=2)
+plt.axhline(y=0, color="k", linestyle="--", linewidth=1)
+
+plt.xticks(range(5))
 plt.xlim((0, 4))
-plt.ylim((-1.5, 3.5))
 plt.xlabel(r"r/$\sigma$")
+
+plt.ylim((-1.5, 3.5))
 plt.ylabel(r"V$_{LJ}$/$\varepsilon$")
 
-plt.axhline(y=0, color="k", linestyle="--", linewidth=1)
-plt.plot(x, lj, linewidth=2)
-
+plt.tight_layout()
 plt.savefig("lj.png", dpi=600)
