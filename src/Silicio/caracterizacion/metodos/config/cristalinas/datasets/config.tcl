e02d2429ffcbd5a2b27d428b605a17d8fced119d
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-12-01 16:58:37 -0300

    explanation with VMD GUI of each tcl command

--- src/Silicio/caracterizacion/metodos/config/cristalinas/datasets/config.tcl
+++ src/Silicio/caracterizacion/metodos/config/cristalinas/datasets/config.tcl
@@ -4,32 +4,68 @@
 #
 # tcl script for configure LiSi systems in VMD, run the following command:
 #   $ vmd -e config.tcl config.lammpstrj
-display projection Orthographic
+#
+# Note: above each tcl command is the explanation of how it would be done in 
+# the VMD GUI.
 
+# Display > Ortographic 
+display projection orthographic
+
+# Display > Axes > Off
 axes location off
 
-color Display Background white
-
+# box must be defined in the lammpstrj file to run this line
 pbc box -color gray
 
+# Graphics > Colors > Categories > Display
+#                   > Names > Background
+#                   > Colors > 8 white
+color Display Background white
+
+# Graphics > Colors > Categories > Name
+#                   > Names > 1
+#                   > Colors > 0 blue
+# edit the RGB of 0 blue with this values
 color change rgb blue 0.121569 0.466667 0.705882
 color Name 1 blue
+
+# Graphics > Colors > Categories > Name
+#                   > Names > 2
+#                   > Colors > 7 green
+# edit the RGB of 7 green with this values
 color change rgb green 0.172549 0.627451 0.172549
 color Name 2 green
 
+# the following 4 cmds allows to make only two representations, in GUI
+# there is one more
 set silicios [atomselect top "type == 1"]
 set litios [atomselect top "type == 2"]
-
 $silicios set radius 0.3
 $litios set radius 0.2
 
+# Insted of delete actual representation, modify:
+# Graphics > Representations >
+#                        Selected Atoms > type 2
+#                        Drawning Method > VDW
+#                          Sphere Scale 0.2
+#                          Sphere Resolution 32
 mol delrep 0 0
 
+# Graphics > Representations > Create Rep >
+#                                  Selected Atoms > type 1
+#                                  Drawning Method > VDW
+#                                    Sphere Scale 0.3
+#                                    Sphere Resolution 32
 mol representation VDW 1.0 32
 mol selection all
 mol addrep 0
 
+# Graphics > Representations > Create Rep >
+#                                  Selected Atoms > type 1
+#                                  Drawning Method > DynamicBonds
+#                                    Distance Cutoff 3.0
+#                                    Bond Radius 0.1
+#                                    Bond Resolution 32
 mol representation DynamicBonds 3.0 0.1 32
-# mol selection {Name Si}
 mol selection {type 1}
 mol addrep 0
--- src/Silicio/modelo/resultados/rdf/datasets/config.tcl
+++ src/Silicio/modelo/resultados/rdf/datasets/config.tcl
@@ -6,26 +6,57 @@
 # position to the simulation cell using periodic boundary conditions, run the 
 # following command:
 #   $ vmd -e config.tcl x-Si.xyz
-display projection Orthographic
+#
+# Note: above each tcl command is the explanation of how it would be done in 
+# the VMD GUI.
 
+# Display > Ortographic 
+display projection orthographic
+
+# Display > Axes > Off
 axes location off
 
+# Graphics > Colors > Categories > Display
+#                   > Names > Background
+#                   > Colors > 8 white
 color Display Background white
 
+# Define the a, b and c of periodic boundary conditions in all frames of the
+# .xyz trajectory (note that this is valid only for simulations with fixed 
+# volume, e.g. NVT ensemble)
 pbc set {10.937456 10.937456 10.937456} -all
+
+# then all the positions are wrapped inside the box
 pbc wrap -all
 
+# and it is displayed with gray color
 pbc box -color gray
 
+# Graphics > Colors > Categories > Name
+#                   > Names > S
+#                   > Colors > 0 blue
+# edit the RGB of 0 blue with this values
 color change rgb blue 0.121569 0.466667 0.705882
 color Name S blue
 
+# Insted of delete actual representation, modify:
+# Graphics > Representations >
+#                        Selected Atoms > all
+#                        Drawning Method > VDW
+#                          Sphere Scale 0.2
+#                          Sphere Resolution 32
 mol delrep 0 0
 
 mol representation VDW 0.2 32
 mol selection all
 mol addrep 0
 
+# Graphics > Representations > Create Rep >
+#                                  Selected Atoms > all
+#                                  Drawning Method > DynamicBonds
+#                                    Distance Cutoff 3.0
+#                                    Bond Radius 0.1
+#                                    Bond Resolution 32
 mol representation DynamicBonds 3.0 0.1 32
 mol selection all
 mol addrep 0
