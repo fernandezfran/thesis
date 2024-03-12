aa1224130a4779d4942c72d1ea04cf73b34d9b22
fernandezfran <ffernandev@gmail.com>
2024-03-11 22:59:01 -0300

    socmax vs crate plots

--- src/FastCharging/un/resultados/ajuste/_socmax_vs_crate_plot.py
+++ src/FastCharging/un/resultados/ajuste/_socmax_vs_crate_plot.py
@@ -4,16 +4,33 @@
 # Copyright (c) 2024, Francisco Fernandez
 # License: CC BY-SA 4.0
 #   https://github.com/fernandezfran/thesis/blob/main/LICENSE
-from shared import *
+import galpynostatic as gp
+import matplotlib.pyplot as plt
+import pandas as pd
 
-index = 3
-X, y = X_data[index], y_data[index]
+plt.rcParams.update({"font.size": 14})
 
-plt.scatter(X, y, marker="s")
+df = pd.read_csv("../datasets/mancini.csv")
+
+X, y = df["crates"].to_numpy().reshape(-1, 1), df["xmaxs"].to_numpy() / 100.0
+
+reg = gp.model.GalvanostaticRegressor("spherical", 7.5e-4, 3)
+reg.fit(X, y)
+
+reg.plot.versus_data(
+    X,
+    y,
+    data_kws={"linestyle": "", "label": "datos experimentales"},
+    pred_kws={"linestyle": "--", "label": "modelo"},
+)
+
+plt.xlim((8e-2, 1.2e1))
+plt.ylim((0.0, 1.0))
 
 plt.xscale("log")
 plt.xlabel("C-rate")
 plt.ylabel(r"$SOC_{\max}$")
+plt.legend()
 
 plt.savefig("_socmax_vs_crate_plot.png", dpi=600)
 plt.show()
